b8daaafab3f8fa01c6fed139dff4a28ba7defe28
Chris Hundhausen <hundhaus@wsu.edu>
2022-02-24 07:57:56 -0800

    Added initial menuBtn click handler to open and close menu

---
+++ scripts/sideMenu.js
@@ -0,0 +1,32 @@
+/*************************************************************************
+ * File: sideMenu.js
+ * These functions support interaction with the side menu.
+ ************************************************************************/
+
+/*************************************************************************
+ * @function menuBtn click handler
+ * @desc 
+ * When the user clicks the menuBtn, open or close the side menu 
+ * based on current menu state.
+ *************************************************************************/
+ document.getElementById("menuBtn")
+   .addEventListener("click", function (e) {
+    const sideMenu = document.getElementById("sideMenu");
+    const sideMenuIcon = document.getElementById("menuBtnIcon");
+    const sideMenuBtn = document.getElementById("menuBtn");
+    if (sideMenuIcon.classList.contains("fa-bars")) { //OPEN MENU
+        //Change menu icon
+        sideMenuIcon.classList.remove("fa-bars");
+        sideMenuIcon.classList.add("fa-times");
+        //Open menu
+        sideMenu.classList.remove("hidden");
+        sideMenuBtn.setAttribute("aria-expanded","true");     
+    } else { //CLOSE MENU
+        //Change menu icon
+        sideMenuIcon.classList.remove("fa-times");
+        sideMenuIcon.classList.add("fa-bars");
+        //Close menu
+        sideMenu.classList.add("hidden");
+        sideMenuBtn.setAttribute("aria-expanded","false");
+    }
+});
