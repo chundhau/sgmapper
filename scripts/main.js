0527c80d97ba19150805d3e189e38a69bfff808b
Chris Hundhausen <hundhaus@wsu.edu>
2022-02-26 07:13:54 -0800

    add keyboard interface to menu

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+/*************************************************************************
+ * File: main.js
+ * Definitions of global variables to maintain app state and provide
+ * convenient access to frequently used DOM elements.
+ *  ************************************************************************/
+
+ const   menuItems = document.querySelectorAll("li[role='menuitem']");
+ let focusedMenuItem = 0; 
+ const menuBtn =  document.getElementById("menuBtn"); 
--- scripts/sideMenu.js
+++ scripts/sideMenu.js
@@ -31,3 +31,62 @@
         sideMenu.classList.remove("sidemenu-open");
     }
 });
+
+/*************************************************************************
+* @function keyDownMenuBtnFocused
+* @desc 
+* Handle keypress when the menuBtn has the focus. Process 
+* the arrow keys, space, and enter. All other keys are ignored.
+* @param key
+* The code of the key that was pressed.
+*************************************************************************/
+function keyDownMenuBtnFocused(key) {
+    if (key === "ArrowDown" || key === "ArrowUp" ||
+            key === "Space" || key === "Enter") {
+            menuBtn.click(); //open menu
+            if (key === "ArrowUp") { //Focus on last item
+                focusedMenuItem = menuItems.length-1;
+            } else { //Focus on first item
+                focusedMenuItem = 0;
+            }
+            menuItems[focusedMenuItem].focus();
+        }
+}
+
+/*************************************************************************
+* @function keyDownMenuItemFocused
+* @desc     
+* Handle keypress when menu is open and an item has focus. Per Table
+* 4.1 from the book, we handle the following key presses: tab, enter
+* escape, up arrow, down arrow, home, and end. 
+* are the arrow keys, space, and enter. All other keys are ignored.
+* @param key
+* The code of the key that was pressed.
+* @globals
+ * focusedMenuitem is the index of the currently focused menu item
+ * menuItems is an array of the HTML elements that are menu items   
+ * menuBtn is a reference to the menu button HTML element
+*************************************************************************/
+function keyDownMenuItemFocused(key) {  
+    if (key == "Enter") { //Activate focused menu item
+        document.activeElement.click();
+    } else if (key === "Tab") { //Close menu
+       menuBtn.click();
+    } else if (key == "Escape") { //Close menu
+        menuBtn.click();
+        menuBtn.focus();
+    } else if (key === "ArrowUp") {  //Focus on next item
+        focusedMenuItem = (focusedMenuItem - 1 + menuItems.length) 
+          % menuItems.length;
+        menuItems[focusedMenuItem].focus();
+    } else if (key === "ArrowDown") {  //Focus on prev item
+        focusedMenuItem = (focusedMenuItem + 1) % menuItems.length;
+        menuItems[focusedMenuItem].focus();
+    } else if (key === "Home") { //Focus on first item
+        focusedMenuItem = 0;
+        menuItems[focusedMenuItem].focus();
+    } else if (key === "End") { //Focus on last item
+        focusedMenuItem = menuItems.length - 1;
+        menuItems[focusedMenuItem].focus();
+    } 
+}
