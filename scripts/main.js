481067afd7ead2c007bd5b9c5d36b24043d195a2
Chris Hundhausen <hundhaus@wsu.edu>
2022-02-26 12:47:17 -0800

    Animate menu when opened and closed

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-/*************************************************************************
- * File: main.js
- * Definitions of global variables to maintain app state and provide
- * convenient access to frequently used DOM elements.
- *  ************************************************************************/
-
- const   menuItems = document.querySelectorAll("li[role='menuitem']");
- let focusedMenuItem = 0; 
- const menuBtn =  document.getElementById("menuBtn"); 
