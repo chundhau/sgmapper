fe69f476b5dea9da9eaf3d94f51ba3e8e0bd577f
Chris Hundhausen <hundhaus@wsu.edu>
2022-07-16 10:05:41 -0700

    Added config file and first menu test

--- tests/menuVisualTests.spec.js
+++ tests/menuVisualTests.spec.js
@@ -1,17 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
-test.describe("menu tests",() => {
-  test.beforeEach(async({ page }) => {
-    await page.goto(''); //goes to base URL set in config file
-    await page.locator('#menuBtn').click();
-  });
-
-  test('open menu', async ({ page }) => { 
-    await expect(page.locator('#sideMenu')).toBeVisible();
-  });
-
-  test('close menu', async ({ page }) => {
-    await page.locator('#menuBtn').click();
-    await expect(page.locator('#sideMenu')).not.toBeVisible();
-  });
+test('open menu', async ({ page }) => { 
+  await page.goto(''); //goes to base URL set in config file
+  await page.locator('#menuBtn').click();
+  await expect(page.locator('#sideMenu')).toBeVisible();
 });
